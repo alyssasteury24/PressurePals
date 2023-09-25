352e74f262f39af108e0633ca72190f9e2b99a32
alyssasteury24 <alyssasteury24@gmail.com>
2023-09-25 18:05:17 -0400

    Add project files.

---
+++ PressurePals/PressurePals/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using PressurePals.ViewModels;
+using PressurePals.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace PressurePals
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ PressurePals/PressurePals/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PressurePals.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PressurePals.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
